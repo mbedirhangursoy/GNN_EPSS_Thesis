eb9b219b665ec2e7ca92db00984fd5c4c8549f7e
Mehmet Bedirhan Gursoy <118937430+mbedirhangursoy@users.noreply.github.com>
2025-05-19 23:49:14 +0200

    import changed

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
 from gnn_final_implementation import *
-from data_related.get_epss_score import get_epss_scores
 
 
 model = HeteroGNN(hidden_dim=32, out_dim=1, metadata=data.metadata())
 
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-print('getting epss scores')
-epss_scores = get_epss_scores(list(data_values.keys()), 'epss_score.csv')
-data_values2 = data_values
-print(len(data_values))
-new_epss_scores = []
-new_data_values = {}
-
-for score, (key, value) in zip(epss_scores, data_values.items()):
-    if score is not None:
-        new_epss_scores.append(score)
-        new_data_values[key] = value
-    else:
-        print(f'removed the following EPSS and CVE-ID: {score}, {key}')
-
-epss_scores = new_epss_scores
-data_values = new_data_values
-
-print(len(data_values))
 
 target = torch.tensor(epss_scores, dtype=torch.float)
 
