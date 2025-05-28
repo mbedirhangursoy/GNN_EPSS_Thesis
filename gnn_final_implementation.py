9e81aca6a9d5b985d364feec5bde74595bd45588
Mehmet Bedirhan Gursoy <118937430+mbedirhangursoy@users.noreply.github.com>
2025-05-29 00:06:57 +0200

    tired again

--- gnn_final_implementation.py
+++ gnn_final_implementation.py
@@ -6,7 +6,10 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from data_related.get_epss_score import *
 
-remove_empty_epss_scores(2025, 2025)
+print('hello')
+tired = remove_empty_epss_scores(2025, 2025)
+
+print(tired)
 '''
 with open('data_related/h_gnn_output.json') as data_values:
     data_values = json.load(data_values)
