855cbc07bda3543256abedb6b35fc732b01bb02b
Mehmet Bedirhan Gursoy <118937430+mbedirhangursoy@users.noreply.github.com>
2025-05-31 11:49:13 +0200

    update

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def evaluate_epss_prediction(mask):
         if t > 0.8:
             threshold = 0.1
         elif t < 0.1:
-            threshold = 0.05
+            threshold = 0.04
         else:
             threshold = 0.1 * t  # 10%
         print(diff, threshold)
