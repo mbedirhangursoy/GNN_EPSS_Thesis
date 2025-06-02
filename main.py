eefca2f2e97da9e07eea327517ef58cced3f8883
Mehmet Bedirhan Gursoy <118937430+mbedirhangursoy@users.noreply.github.com>
2025-06-02 12:23:54 +0200

    update

--- main.py
+++ main.py
@@ -89,10 +89,11 @@ def evaluate_logarithmic_multiclass_prediction(mask):
     pred_classes = []
 
     for t, o in zip(actual, pred):
-        t = t.item()
-        o = o.item()
-        actual_classes.append(create_classes(t).item())
-        pred_classes.append(create_classes(o).item())
+        t = create_classes(t.item())
+        o = create_classes(o.item())
+        print(t, o)
+        actual_classes.append(t)
+        pred_classes.append(o)
 
 
     print(actual_classes, pred_classes)
