37cecb3fd5b1c8118b302ccfc2f07e1ade457f5e
Mehmet Bedirhan Gursoy <118937430+mbedirhangursoy@users.noreply.github.com>
2025-06-06 13:36:00 +0200

    update

--- main.py
+++ main.py
@@ -159,9 +159,9 @@ def test(mask, start_year):
 
 for epoch in range(1, 101):
     loss = train()
-    validation_mse = test(data['label'].validation_mask)
+    #validation_mse = test(data['label'].validation_mask, 2024)
     test_mse = test(data['label'].test_mask, 2024)
-    print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Validation MSE: {validation_mse:.4f}, Test MSE: {test_mse:.4f}')
+    print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Validation MSE: {test_mse:.4f}, Test MSE: {test_mse:.4f}')
     print(evaluate_epss_prediction(data['label'].test_mask))
 
 '''accuracy_log, classification_log, confusion_log = evaluate_logarithmic_multiclass_prediction(data['label'].test_mask, 2024)
