29fbbfb34d39d9f8180285230cfb52987c5ccb45
Mehmet Bedirhan Gursoy <118937430+mbedirhangursoy@users.noreply.github.com>
2025-06-06 13:55:12 +0200

    update main

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class HeteroGNN(torch.nn.Module):
 data = torch.load('data_related/my_final_graph.pt', weights_only=False)
 
 epss_scores = []
-with open('epss_score_final.csv') as csvfile:
+with open('epss_score_2024_2025_deleted.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
         epss_scores.append(float(row[1]))
