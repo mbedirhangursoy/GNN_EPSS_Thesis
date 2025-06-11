b4d02102ae3d5432f0ebc63a1163a27121c296c0
Mehmet Bedirhan Gursoy <118937430+mbedirhangursoy@users.noreply.github.com>
2025-06-11 12:27:47 +0200

    zoomed in to 0.1

--- main.py
+++ main.py
@@ -192,7 +192,7 @@ with open('test_logarithmic_actual_pred_output_2025.csv', 'r') as f:
 
 plt.figure(figsize=(10, 6))
 plt.scatter(actual, predicted, color='blue', alpha=0.6, edgecolors='k', label='Actual vs. Predicted')
-plt.plot([0, 1], [0, 1], 'r--', label='Perfect Prediction')  # Diagonal line
+plt.plot([0, 0.1], [0, 0.1], 'r--', label='Perfect Prediction')  # Diagonal line
 
 plt.xlim(0, 0.1)
 plt.ylim(0, 0.1)
@@ -205,6 +205,6 @@ plt.grid(True)
 plt.tight_layout()
 
 
-plt.savefig('actual_vs_predicted_2025_0.01_to0.1.png', dpi=300)
+plt.savefig('actual_vs_predicted_2025_0.01_zoomedto0.1.png', dpi=300)
 plt.show()
 
