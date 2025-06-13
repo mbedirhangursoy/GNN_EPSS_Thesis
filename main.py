38df81d71cc4b20ef475bdeff4d4f3c60620c5ae
Mehmet Bedirhan Gursoy <118937430+mbedirhangursoy@users.noreply.github.com>
2025-06-13 20:16:44 +0200

    something

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class HeteroGNN(torch.nn.Module):
         return out
 
 
-data = torch.load('data_related/my_graph.pt', weights_only=False)
+data = torch.load('data_related/my_final_graph_updated.pt', weights_only=False)
 
 epss_scores = []
 with open('epss_score_2025_deleted.csv') as csvfile:
