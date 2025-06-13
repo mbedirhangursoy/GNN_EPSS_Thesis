967871a13d6ac6a88131bd54912b52750981fb04
Mehmet Bedirhan Gursoy <118937430+mbedirhangursoy@users.noreply.github.com>
2025-06-13 19:37:38 +0200

    different optimiser

--- main.py
+++ main.py
@@ -45,7 +45,9 @@ with open('epss_score_2025_deleted.csv') as csvfile:
 model = HeteroGNN(hidden_dim=64, out_dim=1, metadata=data.metadata())
 
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+#optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
 
 
 label_node_count = data['label'].num_nodes
